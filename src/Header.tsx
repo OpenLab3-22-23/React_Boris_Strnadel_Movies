2d3bcbb4470a95eaaf61df22ed094fda29f416c2
z20ist01 <z20ist01@z00p3-13.SPSKNM>
2022-11-29 09:47:08 +0100

    ok

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+
+export default function Header()
+{
+    return (
+        <div className="header">
+            <span className="main-title">
+			Movies
+	        </span>
+            <span>  <input type="text" className="search" placeholder="Search"  />  </span>
+        
+            
+        </div>
+        
+    )
+}
