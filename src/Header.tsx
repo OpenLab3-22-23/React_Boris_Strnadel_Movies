5bfc95c277eab6cd7acb984372517eaf91c9c483
z20ist01 <z20ist01@z00p3-13.SPSKNM>
2022-12-13 09:43:54 +0100

    searchbar

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 
 export default function Header()
-{
+{   
+    
+
+
+    const [value, setValue] = useState("")
     return (
         <div className="header">
             <span className="main-title">
 			    Movies
 	        </span>
             <span>  
-                <input type="text" className="search" placeholder="Search"  />  
+                <input type="text" className="search" placeholder="Search"value={value} onChange={e=>{setValue(e.target.value); console.log(value)}}  />  
             </span>
             
         
--- src/Movie.tsx
+++ src/Movie.tsx
@@ -1,3 +1,11 @@
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+
 import { useState } from "react"
 export default function Movie()
 {
@@ -17,3 +25,4 @@ export default function Movie()
 
     
 }
+
